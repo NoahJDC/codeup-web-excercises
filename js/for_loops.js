308569df193f42a61668f4d74bca59e5ba9b7854
Noah Dela Cruz <noahjdc92@gmai.com>
2018-03-23 08:45:57 -0500

    added horizontal line

---
+++ js/for_loops.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function showMultiplicationTable(y){
+    var x = 1;
+    while(x <= 10){
+        console.log( x + ' * ' + y +  " = " + (x * y));
+        x++;
+    }
+}
+showMultiplicationTable(2);
+
+
+
+function oddOrEven() {
+
+    for (var i = 0; i < 10; i++) {
+        var randomNumber = Math.floor(Math.random() * 20) + 180;
+        if ((randomNumber % 2) === 0) {
+        console.log(randomNumber + ' ' + "are evens");
+        } else {
+            console.log(randomNumber + ' ' + "are odds");
+        }
+    }
+}
+oddOrEven();
+
+for (var i = 1; i <= 9; i++) {
+    var string = i.toString();
+    var output = " ";
+    for (var j = 0; j < i; j++ ){
+        output += string;
+    }
+console.log(output);
+}
