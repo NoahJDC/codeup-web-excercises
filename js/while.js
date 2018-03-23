cea993da1b512d20611cb0a4628845f582a69f15
Noah Dela Cruz <noahjdc92@gmai.com>
2018-03-23 11:51:46 -0500

    while lesson completed

--- js/while.js
+++ js/while.js
@@ -10,7 +10,13 @@ while (y <= 65536){
 }
 
 var allCones = Math.floor(Math.random() * 50) + 50;
-
 do{
-
-}
+    var conesSold = Math.floor(Math.random() * 5) + 1;
+    if(conesSold<= allCones) {
+        console.log(conesSold + ' cones sold... ');
+        allCones -= conesSold;
+    } else {
+        console.log('Cannot sell you ' + conesSold + '  cones I only have ' + allCones + '...');
+    }
+} while(allCones > 0);
+console.log('Yay! I sold them all!')
