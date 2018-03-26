becae07f4c2fc49ba717de189d621af50c7fd626
Noah Dela Cruz <noahjdc92@gmai.com>
2018-03-26 09:29:03 -0500

    final

--- js/for_loops.js
+++ js/for_loops.js
@@ -14,7 +14,7 @@ showMultiplicationTable(2);
 function oddOrEven() {
 
     for (var i = 0; i < 10; i++) {
-        var randomNumber = Math.floor(Math.random() * 20) + 180;
+        var randomNumber = Math.floor(Math.random() * 180) + 20;
         if ((randomNumber % 2) === 0) {
         console.log(randomNumber + ' ' + "are evens");
         } else {
@@ -24,11 +24,22 @@ function oddOrEven() {
 }
 oddOrEven();
 
-for (var i = 1; i <= 9; i++) {
-    var string = i.toString();
-    var output = " ";
-    for (var j = 0; j < i; j++ ){
-        output += string;
+function Repeat() {
+    for (var i = 1; i <= 9; i++) {
+        var string = i.toString();
+        var output = " ";
+        for (var j = 0; j < i; j++) {
+            output += string;
+        }
+        console.log(output);
     }
-console.log(output);
 }
+Repeat();
+
+function reverse(){
+    for (var x=100; x >0 ; x-=5){
+        console.log(x)
+    }
+}
+reverse();
+
