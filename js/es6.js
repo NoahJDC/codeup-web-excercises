6f19ba3e2434f602e718833d5be04c3ea06bfc64
Noah Dela Cruz <noahjdc92@gmai.com>
2018-04-19 11:38:39 -0500

    removed return and added closing parenthesis

--- js/es6.js
+++ js/es6.js
@@ -49,12 +49,12 @@ let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(user => {
-  return emails.push(user.email);
-});
-users.forEach(user => {
-  return names.push(user.name);
-});
+users.forEach(user =>
+   emails.push(user.email));
+
+users.forEach(user =>
+    names.push(user.name));
+
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
